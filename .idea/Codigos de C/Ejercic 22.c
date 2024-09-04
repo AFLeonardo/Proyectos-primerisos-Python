26a474f4a9995bc2036ea6158a7b045d4f49d48e
Leonardo <80407889+AFLeonardo@users.noreply.github.com>
2024-09-04 10:50:10 -0600

    Update

--- .idea/Codigos de C/Ejercic 22.c
+++ .idea/Codigos de C/Ejercic 22.c
@@ -11,14 +11,32 @@ struct Estudiante{
 main()
 {
     char agregar;
+    int i = 0;
     Estudiante Datos[20]; //Maximo 20 estudiantes
 
     printf("Quieres agregar datos?: ");
     fflush(stdio);
     scanf("%c", &agregar);
 
-    while(agregar == 's')
+    while(agregar == 's' or i < 20)
     {
-        
+        printf({"Ingresa tu nombre: /n"});
+        fflush(stdio)
+        gets(Datos[i].Nombre)
+
+        printf("Ingresa tu matricula: /n");
+        gets(Datos[i].Matricula);
+
+        printf("En que semestre te encuentras? /n");
+        gets(Datos[i].Semestre)
+
+        for (j = 0 ; j < Semestre ; j++)
+        {
+            printf("Ingresa el promedio del semestre %d/n", j + 1);
+            gets(Datos[i].Promedio[j]);
+        }
+
+        printf("Ingresa tu carrera: /n");
+        gets()
     }
 }
